56e5084f14efec2c1395ce62b7d036df5796995a
CrazyFluff <=>
2023-07-29 14:13:05 -0700

    fix: read from .env

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -9,8 +9,13 @@ import (
 )
 
 func init() {
+	envFile := os.Getenv("ENV_FILE")
+	if envFile == "" {
+		envFile = ".env"
+	}
+
 	// Load the .env file in the current directory
-	err := godotenv.Load()
+	err := godotenv.Load(envFile)
 	if err != nil {
 		log.Printf("An error occurred while trying to load the .env file: %v", err)
 	}
@@ -20,7 +25,7 @@ func GetEnvVar(name string) string {
 	envVar := os.Getenv(name)
 	if envVar == "" {
 		debug.PrintStack()
-		log.Printf("env var %v must be set", name)
+		log.Printf("env var %v must be set\n", name)
 	}
 	return envVar
 }
